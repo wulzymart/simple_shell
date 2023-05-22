f3e1efe27ff31aa8829df531ae7b61eb49b8c5da
timothyVoca <vocalync@gmail.com>
2023-05-22 23:21:16 +0000

    develop new error handler

---
+++ print_error.c
@@ -0,0 +1,88 @@
+#include "main.h"
+
+/**
+ * _putchar_e - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1
+ * on error, -1 is returned, and error is set appropriately
+ */
+int _putchar_e(char c)
+{
+	return (write(2, &c, 1));
+}
+
+/**
+ * _puts_e - prints string followed by new line
+ * @str: string to print
+ * Return: return number of characters printed
+ */
+int _puts_e(const char *str)
+{
+	int i = 0, c = 0;
+
+	while (*(str + i))
+	{
+		c += _putchar_e(*(str + 1));
+		i++;
+	}
+	c += _putchar_e('\n');
+	return (c);
+}
+
+/**
+ * _put_e - prints string but not followed by new line
+ * @str: string to print
+ * Return: return number of characters printed
+ */
+int _put_e(const char *str)
+{
+	int i = 0, c = 0;
+
+	while (*(str + 1))
+	{
+		c += _putchar_e(*(str + 1));
+		i++;
+	}
+	return (c);
+}
+
+/**
+ * print_digits_e - print any given number8
+ * Description: print any number
+ * @n: number to print
+ * Return: void
+ */
+
+void print_digits_e(unsigned int n)
+{
+	if (n < 10)
+		_putchar_e(n + '0');
+	else
+	{
+		if ((n / 10) < 10)
+		{
+			_putchar_e(n / 10 + '0');
+			_putchar_e(n % 10 + '0');
+		}
+		else
+		{
+			print_digits_e(n / 10);
+			_putchar_e(n % 10 + '0');
+		}
+	}
+}
+
+/**
+ * print_e - prints a evector
+ * @av: vector
+ */
+void print_e(char **av)
+{
+	int i;
+
+	for (i = 0; av[i]; i++)
+	{
+		_puts_e(av[i]);
+	}
+}
